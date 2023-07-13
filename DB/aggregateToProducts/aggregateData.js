c0fd499f6d5969f7fffae6f7f91372b63553d0b6
Tim Engle <timothy.englew@gmail.com>
2023-07-13 14:44:17 -0700

    Set up connection to db

---
+++ DB/aggregateToProducts/aggregateData.js
@@ -0,0 +1,16 @@
+const createPhotosAnswers = require('./createPhotosAnswers');
+const createQuestionAnswerPhoto = require('./createQuestionAnswerPhoto');
+const createCompleteProduct = require('./createCompleteProduct');
+
+// run me
+
+const aggregateData = async () => {
+  // console.time('Total Time');
+  await createPhotosAnswers();
+  await createQuestionAnswerPhoto();
+  await createCompleteProduct();
+  // console.timeEnd('Total Time');
+};
+aggregateData().catch(console.error);
+
+aggregateData();
