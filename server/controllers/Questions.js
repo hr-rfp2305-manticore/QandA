fe6741e22b69cbbc91c647183765521397d0d218
Tim Engle <timothy.englew@gmail.com>
2023-07-14 09:55:01 -0700

    Create API request and query for questions based on product_id

--- server/controllers/Questions.js
+++ server/controllers/Questions.js
@@ -1,5 +1,19 @@
+const { Questions } = require('../models');
+
 module.exports = {
-  get: (req, res) => {
-    res.send('Hi!');
+  get: async (req, res) => {
+    try {
+      const { product_id, page, count } = req.body;
+      console.log(req.body);
+      const data = await Questions.getQuestions(
+        Number.parseInt(product_id),
+        Number.parseInt(page),
+        Number.parseInt(count)
+      );
+      res.send(data);
+    } catch (err) {
+      console.error(err);
+      res.status(400).send(err);
+    }
   },
 };
---
+++ server/models/Questions.js
@@ -0,0 +1,28 @@
+const connectDb = require('../db');
+
+module.exports = {
+  getQuestions: async (product_id, page = 1, count = 5) => {
+    try {
+      const db = await connectDb();
+      const productCollection = db.collection('QuestionAnswerPhoto');
+
+      const data = await productCollection
+        .find({ product_id: product_id }, { projection: { product_id: 0 } })
+        .limit(count)
+        .skip((page - 1) * count)
+        .toArray();
+      console.log(data);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  // getQuestions: async (product_id, page = 1, count = 5) => {
+  //   try {
+  //     const db = await connectDb();
+  //     const questionDatabase = db.collection('Question');
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // },
+};
