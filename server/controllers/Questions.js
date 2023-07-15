60e834da20a1d6bfe8e87a93536011d26e83c21d
Tim Engle <timothy.englew@gmail.com>
2023-07-14 23:23:10 -0700

    Insert new Question POST

--- server/controllers/Questions.js
+++ server/controllers/Questions.js
@@ -1,14 +1,17 @@
 const { Questions } = require('../models');
+// const { param } = require('../routes');
 
 module.exports = {
   get: async (req, res) => {
+    let { product_id, page, count } = req.body;
+    count = checkInput(count, 5);
+    page = checkInput(page, 1);
+
     try {
-      const { product_id, page, count } = req.body;
-      console.log(req.body);
       const data = await Questions.getQuestions(
         Number.parseInt(product_id),
-        Number.parseInt(page),
-        Number.parseInt(count)
+        page,
+        count
       );
       res.send(data);
     } catch (err) {
@@ -19,10 +22,30 @@ module.exports = {
 
   post: async (req, res) => {
     const { body, name, email, product_id } = req.body;
+
     console.log(body);
     console.log(name);
     console.log(email);
     console.log(product_id);
-    res.status(201).send('You are posting this');
+    try {
+      const data = await Questions.postQuestion(
+        Number.parseInt(product_id),
+        body,
+        name,
+        email
+      );
+      res.status(201).send(data);
+    } catch (err) {
+      console.error(err);
+      res.status(400).send(err);
+    }
   },
+};
+
+const checkInput = (parameter, defaultAmount) => {
+  if (!parameter) {
+    return defaultAmount;
+  } else {
+    return Number.parseInt(parameter);
+  }
 };
--- server/models/Questions.js
+++ server/models/Questions.js
@@ -1,20 +1,66 @@
 const connectDb = require('../db');
+const buffer = [];
 
 module.exports = {
-  getQuestions: async (product_id, page = 1, count = 5) => {
+  getQuestions: async (product_id, page, count) => {
+    const skipTo = (page - 1) * count;
     try {
       const db = await connectDb();
       const productCollection = db.collection('QuestionAnswerPhoto');
+      console.log(count, page);
+      // Uncomment this line to increase query speed
+      // productCollection.createIndex({ product_id: 1 });
 
       const data = await productCollection
         .find({ product_id: product_id }, { projection: { product_id: 0 } })
-        .limit(count)
-        .skip((page - 1) * count)
+        .limit(5)
+        .skip(skipTo)
         .toArray();
-      console.log(data);
+      // console.log(data);
       return data;
     } catch (err) {
       console.error(err);
+      throw err;
     }
   },
+  postQuestion: async (product_id, body, name, email) => {
+    try {
+      const db = await connectDb();
+      const questionsCollection = db.collection('QuestionAnswerPhoto');
+      const collectionLength = await questionsCollection.countDocuments({});
+
+      const document = {
+        answers: [],
+        asker_email: email,
+        asker_name: name,
+        product_id: product_id,
+        question_body: body,
+        question_helpfullness: 0,
+        question_id: collectionLength + 1,
+        reported: false,
+      };
+
+      const result = await questionsCollection.insertOne(document);
+      return result;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  },
+  // Future Buffer set up
+  // postQuestion: async (product_id, body, name, email) => {
+  //   const document = {
+  //     answers: [],
+  //     asker_email: email,
+  //     asker_name: name,
+  //     product_id: product_id,
+  //     question_body: body,
+  //     question_helpfullness: 0,
+  //     question_id: buffer.length,
+  //     reported: false,
+  //   };
+  //   buffer.push(document);
+  //   console.log(buffer);
+  //   return document;
+  // },
 };
