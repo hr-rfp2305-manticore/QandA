a608b7afa4f67307ada45651e994aac75e6f92c6
Tim Engle <timothy.englew@gmail.com>
2023-07-14 17:23:22 -0700

    Remove old version files

--- DB/aggregateToProducts/aggregateData.js
+++ DB/aggregateToProducts/aggregateData.js
@@ -2,14 +2,10 @@ const createPhotosAnswers = require('./createPhotosAnswers');
 const createQuestionAnswerPhoto = require('./createQuestionAnswerPhoto');
 const createCompleteProduct = require('./createCompleteProduct');
 
-// run me
-
 const aggregateData = async () => {
-  // console.time('Total Time');
   await createPhotosAnswers();
   await createQuestionAnswerPhoto();
   // await createCompleteProduct();
-  // console.timeEnd('Total Time');
 };
 aggregateData().catch(console.error);
 
