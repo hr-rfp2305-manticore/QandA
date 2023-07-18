1f6ec8e85ab251c5b2276f4f15d4d3f6a5bfd616
Tim Engle <timothy.englew@gmail.com>
2023-07-18 12:20:08 -0700

    ➕add runScript function

--- DB/aggregateToProducts/aggregateData.js
+++ DB/aggregateToProducts/aggregateData.js
@@ -13,12 +13,19 @@ const addIndexes = async () => {
     const db = client.db(DB_NAME);
     const answerPhotosCollection = db.collection('AnswerAndPhotos');
     const questionsCollection = db.collection('Questions');
-
+    console.log('Creating Indexes....');
     await answerPhotosCollection.createIndex({ question_id: 1 });
+    console.log('Completed 1/5 Indexes');
     await answerPhotosCollection.createIndex({ id: 1 });
+    console.log('Completed 2/5 Indexes');
     await questionsCollection.createIndex({ id: 1 });
+    console.log('Completed 3/5 Indexes');
     await questionsCollection.createIndex({ product_id: 1 });
+    console.log('Completed 4/5 Indexes');
     await questionsCollection.createIndex({ reported: 1 });
+    console.log('Completed 5/5 Indexes');
+    console.log('Successfully created all Indexes!');
+    client.close();
   } catch (err) {
     console.error(err);
   }
@@ -26,11 +33,18 @@ const addIndexes = async () => {
 
 const aggregateData = async () => {
   try {
+    console.log('Creating Collection....');
+
     await createPhotosAnswers();
+    console.log('Successfully created Collection!');
   } catch (err) {
     console.err(err);
   }
 };
 
-aggregateData();
-addIndexes();
+const runScript = async () => {
+  await aggregateData();
+  await addIndexes();
+};
+
+runScript();
