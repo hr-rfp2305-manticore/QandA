f5f693d4738bcd0d30eeb3721f60ca3859136480
Tim Engle <timothy.englew@gmail.com>
2023-08-09 21:58:07 -0700

    🧹 Removed console.log statements

--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./controllers');
 
+// Questions
 router.get('/:product_id', controllers.Questions.get);
 router.post('/', controllers.Questions.post);
 router.put('/:question_id/helpful', controllers.Questions.putHelp);
 router.put('/:question_id/report', controllers.Questions.putReport);
 
+// Answers
 router.get('/:question_id/answers', controllers.Answers.get);
 router.post('/:question_id/answers', controllers.Answers.post);
 router.put('/answers/:answer_id/helpful', controllers.Answers.putHelp);
@@ -14,4 +16,5 @@ router.put('/answers/:answer_id/report', controllers.Answers.putReport);
 
 router.get('/test/:question_id', controllers.Questions.readTest);
 router.get('/answers/test/:answer_id', controllers.Answers.readTest);
+
 module.exports = router;
